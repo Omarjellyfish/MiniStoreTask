f63fc15a88cfd1e4927747155fb546292bc611d4
Omarjellyfish <omarkandilfan@gmail.com>
2024-09-20 01:35:43 +0300

    Project intialization

---
+++ MiniStore/index.js
@@ -0,0 +1,17 @@
+// const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
+// const preBtn = [...document.querySelectorAll(".pre-btn")];
+// const productContainers = [...document.querySelectorAll(".image-container")];
+
+// productContainers.forEach((item, i) => {
+//   let containerDimensions = item.getBoundingClientRect();
+//   let containerWidth = containerDimensions.width;
+
+//   nxtBtn[i].addEventListener("click", () => {
+//     item.scrollLeft += containerWidth;
+//   });
+
+//   preBtn[i].addEventListener("click", () => {
+//     item.scrollLeft -= containerWidth;
+//     console.log("pre");
+//   });
+// }); scrolling with button
