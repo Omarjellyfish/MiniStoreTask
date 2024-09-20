eebe6fa7ba9592f899e33997b69861730c702892
Omarjellyfish <omarkandilfan@gmail.com>
2024-09-20 22:06:07 +0300

    Responsive changes

--- MiniStore/index.js
+++ MiniStore/index.js
@@ -15,3 +15,20 @@
 //     console.log("pre");
 //   });
 // }); scrolling with button
+
+const navBtn = document.getElementById("nav-btn");
+const dropNav = document.getElementById("dropNav");
+const closeBtn = document.getElementById("close-btn");
+const phoneNav = document.getElementById("pn");
+const mainContainer = document.getElementsByClassName("container")[0];
+navBtn.addEventListener("click", () => {
+  dropNav.classList.add("show");
+  phoneNav.style.display = "none";
+  mainContainer.style.display = "none";
+});
+
+closeBtn.addEventListener("click", () => {
+  dropNav.classList.remove("show");
+  phoneNav.style.display = "flex";
+  mainContainer.style.display = "block";
+});
